b8a5b95c6d79fd621aebc1a0ef583506de4ae455
Pauli Nieminen <suokkos@gmail.com>
2014-01-30 02:15:16 +0200

    Implement parallel library generation
    With modern multi core machines parallel operation is a lot faster
    specially if parallel code can be run lock free. There is simple
    solution of starting multiple dds programs same time and using select to
    know which one of them has data available.
    
    Data needs to be split to small chucks to maximize core utilisation.
    With large chunks some process might take far longer than other to
    completely their share of work.
    
    Signed-off-by: Pauli Nieminen <suokkos@gmail.com>

--- genlib.c
+++ genlib.c
@@ -8,6 +8,12 @@
 #include <time.h>
 #include <arpa/inet.h>
 #include <limits.h>
+#include <fcntl.h>
+
+#include <assert.h>
+
+#include <sys/select.h>
+#include <errno.h>
 
 static void usage(const char *name, int exitcode, const char *msg, ...)
 {
@@ -22,11 +28,22 @@ static void usage(const char *name, int exitcode, const char *msg, ...)
       "\t-a <file>\tAppend output to the file\n"
       "\t-g <number>\tSet number of deals to generate <100>\n"
       "\t-s <number>\tSelect output file <current time>\n"
+      "\t-c <number>\tSet number of threads to use <a thread per core>\n"
       "\t-q\tBe quiet\n"
       "\t-v\tBe verbose\n",
       name);
   exit(exitcode);
 }
+
+struct process {
+  FILE *f;
+  int fd;
+  char *partial;
+};
+
+static unsigned long generate = 0;
+static unsigned long scheduled = 0;
+static int linefeed = 0;
 
 static char ucrep[14] = "23456789TJQKA";
 
@@ -57,10 +74,12 @@ static void parsedeal(struct tagLibdeal *d,
       val[0] = result[cont*4 + p];
       tricksew = 13 - strtol(val, NULL, 16);
       d->tricks[suit] |= tricksew << (leader_to_player[p]*4);
-      if (verbosity >= 3)
-        fprintf(stderr, "contract %c makes %ld tricks for ew by %d -> %x\n",
+      if (verbosity >= 3) {
+        fprintf(stderr, "%scontract %c makes %ld tricks for ew by %d -> %x\n", (linefeed ? "\n": ""),
             contracts[suit], tricksew, leader_to_player[p],
             d->tricks[suit]);
+        linefeed = 0;
+      }
     }
   }
 
@@ -76,7 +95,7 @@ static void parsedeal(struct tagLibdeal *d,
       rank = 12 - (strchr(ucrep, player[p][c]) - ucrep);
       d->suits[3 - suit] |= p << (rank * 2);
       if (verbosity >= 3)
-        fprintf(stderr, "card %d in suit %d belongs to %d -> %x\n",
+        fprintf(stderr, "%scard %d in suit %d belongs to %d -> %x\n", (linefeed ? "\n": ""),
             rank, 3 - suit, p, d->suits[3 - suit]);
     }
   }
@@ -87,28 +106,136 @@ static void parsedeal(struct tagLibdeal *d,
     d->tricks[p] = htons(d->tricks[p]);
 }
 
-int main(int argc, char * const argv[])
+static void parseLine(char *buffer, FILE *out, struct process *p, unsigned long gen, int verbosity)
 {
-  char *output = NULL;
-  char ddscmd[256];
-  char buffer[1024];
-  unsigned long gen = 100, seed;
-  char c;
-  FILE *in, *out;
-  int verbosity = 1;
-  char mode[] = "w";
   const char * const filenamestart = "output written to file ";
   const char * const skipdeals = "  deal=";
 
   char north[17], east[17], south[17], west[17], results[21];
   long deal;
 
+  if (!strchr(buffer, '\n')) {
+    assert(p->partial == NULL);
+    p->partial = strdup(buffer);
+    return;
+  }
+
+  if (p->partial) {
+    char *end = memchr(p->partial, '\0', 256);
+    long len = end - p->partial;
+    memmove(buffer + len, buffer, (char *)memchr(buffer, '\0', 256) - buffer);
+    memcpy(buffer, p->partial, len);
+    free(p->partial);
+    p->partial = NULL;
+  }
+
+  if (strncmp(buffer, skipdeals, strlen(skipdeals)) == 0)
+    return;
+  if (strncmp(buffer, filenamestart, strlen(filenamestart)) == 0) {
+    const char * const outfile = buffer + strlen(filenamestart);
+    strchr(outfile, '\n')[0] = '\0';
+    if (verbosity >= 2) {
+      fprintf(stderr, "%sRemoving '%s'\n", (linefeed ? "\n": ""), outfile);
+      linefeed = 0;
+    }
+    remove(outfile);
+    return;
+  }
+  if (sscanf(buffer, "%ld %16s %16s %16s %16s:%20s",
+        &deal, west, north, east, south, results) == 6) {
+    struct tagLibdeal libdeal = {{0}};
+    parsedeal(&libdeal, north, east, south, west, results, verbosity);
+
+    if (fwrite(&libdeal, 26, 1, out) <= 0) {
+      fprintf(stderr, "%sFailed to write libdeal entry %ld\n", (linefeed ? "\n": ""), deal);
+      exit(30);
+    }
+
+    generate++;
+
+    if (verbosity >= 1 &&
+        (100 * generate / gen > 100 * (generate - 1) / gen ||
+         generate / 100 > (generate - 1) / 100)) { 
+      fprintf(stderr, "Completed %ld/%ld deals (%ld%%)\r",
+          generate, gen, 100 * generate / gen);
+      linefeed = 1;
+    }
+    return;
+  }
+}
+
+static FILE *nonblockpopen(const char *cmd, const char *mode)
+{
+  int fd;
+  int flags;
+  FILE *r = popen(cmd, mode);
+  if (!r)
+    return r;
+
+  fd = fileno(r);
+
+  flags = fcntl(fd, F_GETFL, 0);
+  flags |= O_NONBLOCK;
+  fcntl(fd, F_SETFL, flags);
+
+  return r;
+}
+
+static int makeprocess(struct process *p, unsigned long gen, unsigned long seed, int verbosity)
+{
+  char ddscmd[256];
+  FILE *f;
+
+  assert(p->f == NULL);
+  sprintf(ddscmd, "gen-%ld-%ld-52-20.txt", seed, gen);
+  remove(ddscmd);
+
+  sprintf(ddscmd, "dds -genseed=%ld -gen=%ld -gentricks=20", seed, gen);
+
+  if (verbosity >= 2) {
+    fprintf(stderr, "%sRunning '%s'\n", (linefeed ? "\n": ""), ddscmd);
+    linefeed = 0;
+  }
+
+  f = nonblockpopen(ddscmd, "r");
+
+  if (!f) {
+    perror("Can't run the dds");
+    return - 1;
+  }
+  scheduled += gen;
+  p->f = f;
+  p->fd = fileno(p->f);
+  return p->fd;
+}
+
+static void closeprocess(struct process *p)
+{
+  pclose(p->f);
+  p->f = NULL;
+  p->fd = 0;
+  free(p->partial);
+  p->partial = NULL;
+}
+
+int main(int argc, char * const argv[])
+{
+  char *output = NULL;
+  char buffer[1024];
+  unsigned long gen = 100, seed;
+  char c;
+  FILE *out;
+  int verbosity = 1, running = 0;
   struct timespec tp;
+  char mode[] = "w";
+  int cores = sysconf (_SC_NPROCESSORS_CONF);
+  unsigned long blocksize;
+  struct process *process;
 
   clock_gettime(CLOCK_REALTIME, &tp);
 
   seed = tp.tv_nsec + tp.tv_sec * 1000 * 1000 * 1000;
-  while ((c = getopt(argc, argv, "ho:s:g:qv")) != -1) {
+  while ((c = getopt(argc, argv, "hc:a:o:s:g:qv")) != -1) {
     switch (c) {
     default:
       usage(argv[0], 1, "ERROR: Unknown parameter %c\n", c);
@@ -125,6 +252,11 @@ int main(int argc, char * const argv[])
     case 'q':
       verbosity = 0;
       break;
+    case 'c':
+      cores = atoi(optarg);
+      if (cores <= 0)
+        usage(argv[0], 2, "ERROR: %d cores must be over zero", cores);
+      break;
     case 'v':
       verbosity++;
       break;
@@ -139,11 +271,29 @@ int main(int argc, char * const argv[])
     }
   }
 
+  freopen("/dev/null", "r", stdin);
+
+  process = alloca(sizeof(process[0])*cores);
+  memset(process, 0, sizeof(process[0])*cores);
+
+  if (gen >= cores * 8 && cores > 1)
+    blocksize = gen / (cores * 4);
+  else if (gen >= cores * 2 && cores > 1)
+    blocksize = gen / (cores * 2);
+  else if (gen >= cores && cores > 1)
+    blocksize = gen / cores;
+  else
+    blocksize = gen;
+
+  if (blocksize >= 40)
+    blocksize = 40;
+
   seed ^= seed >> 31;
   seed &= (uint32_t)~0;
 
   if (verbosity >= 1)
-    fprintf(stderr, "Generating %ld deals with %ld seed\n", gen, seed);
+    fprintf(stderr, "Generating %ld deals with %ld seed. Each subprocess does %ld deals.\n",
+        gen, seed, blocksize);
 
   out = output ? fopen(output, mode) : stdout;
 
@@ -152,49 +302,76 @@ int main(int argc, char * const argv[])
     return 10;
   }
 
-  sprintf(ddscmd, "dds -genseed=%ld -gen=%ld -gentricks=20", seed, gen);
+  {
+    int fd, maxfd = -1;
+    int c;
+    fd_set fds;
+    fd_set rfds;
 
-  if (verbosity >= 2)
-    fprintf(stderr, "Running '%s'\n", ddscmd);
+    FD_ZERO(&fds);
 
-  in = popen(ddscmd, "r");
+    for (c = 0; c < cores; c++) {
+      unsigned long b = gen - scheduled > blocksize ?
+                blocksize : gen - scheduled;
 
-  if (!in) {
-    perror("Can't run the dds");
-    return 20;
-  }
+      if (b == 0)
+        break;
 
-  while (fgets(buffer, sizeof buffer, in)) {
-    if (strncmp(buffer, skipdeals, strlen(skipdeals)) == 0)
-      continue;
-    if (strncmp(buffer, filenamestart, strlen(filenamestart)) == 0) {
-      const char * const outfile = buffer + strlen(filenamestart);
-      strchr(outfile, '\n')[0] = '\0';
-      if (verbosity >= 2)
-        fprintf(stderr, "Removing '%s'\n", outfile);
-      remove(outfile);
-      continue;
+      if ((fd = makeprocess(&process[c], b, seed++, verbosity)) < 0)
+        return 20;
+      running++;
+
+      if (fd > maxfd)
+        maxfd = fd;
+
+      FD_SET(fd, &fds);
     }
-    if (sscanf(buffer, "%ld %16s %16s %16s %16s:%20s",
-          &deal, west, north, east, south, results) == 6) {
-      struct tagLibdeal libdeal = {{0}};
-      parsedeal(&libdeal, north, east, south, west, results, verbosity);
 
-      if (fwrite(&libdeal, 26, 1, out) <= 0) {
-        fprintf(stderr, "Failed to write libdeal entry %ld\n", deal);
-        return 30;
+    while (running > 0) {
+      int active;
+      int p = 0;
+      rfds = fds;
+      active = select(maxfd + 1, &rfds, NULL, NULL, NULL);
+
+      if (active <= 0) {
+        perror("select");
+        return 60;
       }
+      for (; p < cores; p++) {
+        if (FD_ISSET(process[p].fd, &rfds)) {
+          errno = 0;
+          while (fgets(buffer, sizeof buffer, process[p].f))
+            parseLine(buffer, out, &process[p], gen, verbosity);
 
-      if (verbosity >= 1 && 100 * deal / gen > 100 * (deal - 1) / gen)
-        fprintf(stderr, "Completed %ld/%ld deals. Progress is %ld %%\r",
-            deal, gen, 100 * deal / gen);
-      if (verbosity >= 1 && deal == gen)
-        fprintf(stderr, "\n");
-      continue;
+          if (feof(process[p].f)) {
+            FD_CLR(process[p].fd, &fds);
+            closeprocess(&process[p]);
+            running--;
+            if (scheduled < gen) {
+              unsigned long b = gen - scheduled > blocksize ?
+                blocksize : gen - scheduled;
+
+              if ((fd = makeprocess(&process[p], b, seed++, verbosity)) < 0)
+                return 20;
+
+              running++;
+
+              if (fd > maxfd)
+                maxfd = fd;
+
+              FD_SET(fd, &fds);
+            }
+            continue;
+          }
+          if (errno != EWOULDBLOCK) {
+            perror("fgets");
+            return 50;
+          }
+        }
+      }
     }
   }
 
-  pclose(in);
   fclose(out);
   return 0;
 }
