65841bab2a984f328a9a45a72281e0cba8a4b59a
Pauli <suokkos@gmail.com>
2020-06-28 19:25:16 +0300

    Document bit twiddling functions
    Signed-off-by: Pauli <suokkos@gmail.com>

--- bittwiddle.cpp
+++ bittwiddle.cpp
@@ -2,9 +2,18 @@
 
 #include "bittwiddle.h"
 
-
+// The optimization namespace e.g. avx2_
 namespace DEFUN() {
 
+    /**
+     * A fallback function to count set bits. It is only used if compiling
+     * a call to ::popcount without instruction set support. Fallback is
+     * compiled twice to generate instruction optimized version and open coded
+     * bit operation versions for runtime selections.
+     *
+     * @param uv Unsigned integer value to count set bits
+     * @return number of set bits in uv
+     */
     template<typename uT>
     static inline uT popcounti(uT uv)
     {
@@ -35,6 +44,7 @@ namespace DEFUN() {
 		uv = (uv  + (uv >> depth)) & mask;
 		depth *= 2;
 	}
+        // Is the bit count complete after addition steps?
 	if (depth == std::numeric_limits<uT>::digits)
 		return uv;
 
@@ -43,10 +53,15 @@ namespace DEFUN() {
 	return uT(uv * mask) >> (std::numeric_limits<uT>::digits - depth);
     }
 
+    /// Runtime detected 8bit set bit count fallback entry
     auto pc8_register  = make_entry_register(::popcounti<uint8_t>, popcounti<uint8_t>);
+    /// Runtime detected 16bit set bit count fallback entry
     auto pc16_register = make_entry_register(::popcounti<uint16_t>, popcounti<uint16_t>);
+    /// Runtime detected 32bit set bit count fallback entry
     auto pc32_register = make_entry_register(::popcounti<uint32_t>, popcounti<uint32_t>);
+    /// Runtime detected 62bit set bit count fallback entry
     auto pc64_register = make_entry_register(::popcounti<uint64_t>, popcounti<uint64_t>);
+    /// Runtime detected long long set bit count fallback entry
     auto pcll_register = make_entry_register(::popcounti<unsigned long long>, popcounti<unsigned long long>);
 }
 
--- bittwiddle.h
+++ bittwiddle.h
@@ -6,12 +6,20 @@
 
 #include "entry.h"
 
+/// A fallback implementation with runtime detected optimization
 template<typename T>
 entry<T (*)(T)> popcounti;
 
+/**
+ * Return count of set bits.
+ *
+ * @param v Integer which set bits are counted
+ * @return The number of set bits
+ */
 template<typename T>
 static inline T popcount(T v)
 {
+	// Native ALU width might be different than pointer width (e.g. x32)
 #if __GNUC__
 #if __WORDSIZE == 32
 	using native_t = uint32_t;
@@ -19,15 +27,20 @@ static inline T popcount(T v)
 	using native_t = uint64_t;
 #endif
 #else
-	// Native ALU width might not match pointer width on some platforms (like
-	// Linux x32)
 	using native_t = unsigned intptr_t;
 #endif
+
+	// Helper type to convert argument to same width unsigned type
 	using uT = typename std::make_unsigned<T>::type;
+	// Convert to unsigned without changes to bit representation.
 	uT uv = v;
+	// Optimize case when counting bits in a value which is wider than native
+	// register width. This is used on 32bit system when counting cards.
 	if (sizeof(v) > sizeof(native_t)) {
 		native_t rv = 0;
 		unsigned count = sizeof(v)/sizeof(native_t);
+		// Calculate shift width while avoiding warning for undefined behavior
+		// when shifting more than the width of type.
 		const unsigned shift = std::min(std::numeric_limits<native_t>::digits,
 				std::numeric_limits<uT>::digits - 1);
 		do {
@@ -37,9 +50,11 @@ static inline T popcount(T v)
 		return rv;
 	}
 #if !__POPCNT__
+	// Without instruction support call fallback function
 	return popcounti<uT>(uv);
 #endif
 #if __GNUC__
+	// Call type specific counting builtin to generate correct instructions
 	if (sizeof(v) == sizeof(long long))
 		return __builtin_popcountll(uv);
 	if (sizeof(v) == sizeof(long))
@@ -52,6 +67,12 @@ static inline T popcount(T v)
 #endif
 }
 
+/**
+ * Count trailing zero bits.
+ *
+ * @param v Integer which trailing zero bits are counted
+ * @return The number of trailing zero bits
+ */
 template<typename T>
 static inline T ctz(T v)
 {
@@ -71,6 +92,7 @@ static inline T ctz(T v)
 	uT uv = v;
 	unsigned depth = std::numeric_limits<uT>::digits/2;
 	unsigned position = depth;
+	// Binary search for zero bits
 	for (; depth > 1;) {
 		depth /= 2;
 		uT mask = (uT{1} << position) - 1;
@@ -85,6 +107,12 @@ static inline T ctz(T v)
 	return position;
 }
 
+/**
+ * Count leading zero bits
+ *
+ * @Param v Integer which leading zero bits are counted
+ * @return The number of leading zero bits
+ */
 template<typename T>
 static inline T clz(T v)
 {
@@ -105,6 +133,7 @@ static inline T clz(T v)
 	uT uv = v;
 	unsigned depth = std::numeric_limits<uT>::digits/2;
 	unsigned position = depth;
+	// Binary search for zero bits
 	for (; depth > 1;) {
 		depth /= 2;
 		uT mask = (uT{1} << position) - 1;
