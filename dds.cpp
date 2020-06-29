eebc3179cac49d3b6b80011c345e8f64a0906657
Pauli <suokkos@gmail.com>
2020-06-29 17:39:05 +0300

    Improve comments for libdds loader
    Signed-off-by: Pauli <suokkos@gmail.com>

--- dds.cpp
+++ dds.cpp
@@ -21,19 +21,29 @@ static decltype(SolveBoard)* ddSolveBoard = NULL;
 /// Function pointer to helper converting libdds error codes to error text
 static decltype(ErrorMessage)* ddErrorMessage = NULL;
 
+/**
+ * Convert dealer hand representation to libdds representation.
+ */
 static void cardsToDeal(struct deal *dl, const union board *d)
 {
 	unsigned h, s;
 	for (h = 0; h < 4; h++) {
+		// libdds format has bit indexes matching to rank while our format has
+		// rank 2 at bit index 0
 		dl->remainCards[h][3] = (d->hands[h] << 2) & 0xFFFF;
 		for (s = 1; s < 4; s++)
 			dl->remainCards[h][3-s] = (d->hands[h] >> (SUIT_WIDTH*s - 2)) & 0xFFFF;
 	}
 }
 
+/**
+ * Run double dummy analyse for a board and declarer. Implementation for
+ * tricks() function in scripts.
+ */
 static int Solve(const union board *d, int declarer, int contract)
 {
 	int r;
+	// Initialize deal structure requesting solution
 	struct deal dl = {
 		contract < 4 ? 3 - contract : 4,
 		(declarer + 1) % 4,
@@ -44,24 +54,39 @@ static int Solve(const union board *d, int declarer, int contract)
 		{0,0,0,0},
 		{0,0,0,0}},
 	};
+	// Find the best possible trick count
 	int target = -1;
+	// We want only the best solution
 	int solutions = 1;
+	// No reusing tables
 	int mode = 1;
+	// We only have one thread
 	int thread = 0;
+	// Initialize result structure
 	struct futureTricks futp;
 	std::memset(&futp, 0, sizeof(futp));
+	// Copy hands from our format to libdds format in deal structure
 	cardsToDeal(&dl, d);
+	// Do the double dummy analyze
 	r = ddSolveBoard(dl, target, solutions, mode, &futp, thread);
 	if (r != RETURN_NO_FAULT) {
 		char line[80];
+		// Report the error in human readable format
 		ddErrorMessage(r, line);
 		std::fprintf(stderr, "DD error: %s\n", line);
 		return 0;
 	}
+	// Double dummy analyze solves tricks for player on lead. Return value is
+	// expected to tell tricks for declarer so we need to convert the trick
+	// count for opposite side.
 	return 13 - futp.score[0];
 }
 
-static void SolveLead(const union board *d, int declarer, int contract, card cards, char res[13])
+/**
+ * Solve double dummy results for all potential leads. Implementation for
+ * leadtricks() function in scripts.
+ */
+static void SolveLead(const union board *d, int declarer, int contract, hand cards, char res[13])
 {
 	int r;
 	struct deal dl = {
@@ -74,12 +99,17 @@ static void SolveLead(const union board *d, int declarer, int contract, card car
 		{0,0,0,0},
 		{0,0,0,0}},
 	};
+	// Search for the best trick count
 	int target = -1;
+	// Return solutions for all playable cards
 	int solutions = 3;
+	// Avoid reusing cached results
 	int mode = 1;
+	// We have only one thread
 	int thread = 0;
 	struct futureTricks futp;
 	std::memset(&futp, 0, sizeof(futp));
+	// Convert our format to libdds format
 	cardsToDeal(&dl, d);
 	r = ddSolveBoard(dl, target, solutions, mode, &futp, thread);
 	if (r != RETURN_NO_FAULT) {
@@ -88,6 +118,8 @@ static void SolveLead(const union board *d, int declarer, int contract, card car
 		std::fprintf(stderr, "DD error: %s\n", line);
 		return;
 	}
+	// Convert results to tricks possible by declarer and store them in result
+	// array.
 	for (target = 0; target < futp.cards; target++) {
 		card t = MAKECARD(3 - futp.suit[target], futp.rank[target] - 2);
 		if ((t & cards) == 0)
@@ -99,6 +131,11 @@ static void SolveLead(const union board *d, int declarer, int contract, card car
 	}
 }
 
+/**
+ * Loads libdds library dynamically and links SoveBoard to the function pointer.
+ *
+ * Not thread safe.
+ */
 static void loadLib()
 {
 #if !defined(_WIN32)
@@ -133,13 +170,17 @@ static void loadLib()
 	}
 }
 
+/**
+ * Make sure library is loaded and assigned to solve pointer. Then forward call
+ * to the call handling function.
+ */
 static int loadSolve(const union board *d, int declarer, int contract)
 {
 	loadLib();
 	return solve(d, declarer, contract);
 }
 
-static void loadSolveLead(const union board *d, int declarer, int contract, card cards, char res[13])
+static void loadSolveLead(const union board *d, int declarer, int contract, hand cards, char res[13])
 {
 	loadLib();
 	solveLead(d, declarer, contract, cards, res);
@@ -148,4 +189,4 @@ static void loadSolveLead(const union board *d, int declarer, int contract, card
 }
 
 int (*solve)(const union board *d, int declarer, int contract) = dds::loadSolve;
-void (*solveLead)(const union board *d, int declarer, int contract, card cards, char res[13]) = dds::loadSolveLead;
+void (*solveLead)(const union board *d, int declarer, int contract, hand cards, char res[13]) = dds::loadSolveLead;
--- dds.h
+++ dds.h
@@ -5,5 +5,22 @@
 
 union board;
 
+/**
+ * Do double dummy analyze returning number of tricks declarer gets if defenders
+ * select the best opening lead.
+ *
+ * @param d Complete deal to be checked with double dummy
+ * @param declarer The compass direction of declarer
+ * @return Number of tricks declarer makes based on double dummy analyze
+ */
 extern int (*solve)(const union board *d, int declarer, int contract);
-extern void (*solveLead)(const union board *d, int declarer, int contract, card cards, char res[13]);
+/**
+ * Do double dummy analyze and store number of tricks declarer makes for each
+ * card in res array.
+ *
+ * @param d Complete deal to be checked with double dummy
+ * @param declarer The compass direction of declarer
+ * @param cards Cards in the hand which has opening lead
+ * @param res Array for double dummy results
+ */
+extern void (*solveLead)(const union board *d, int declarer, int contract, hand cards, char res[13]);
